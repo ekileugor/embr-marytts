2b38d1076d94dfbc871873a30d78d788c59b5285
gianluca <gianluca@gianluca-laptop.(none)>
2010-09-02 18:51:12 +0200

    added function for nicer ghci interaction

--- src/Test.hs
+++ src/Test.hs
@@ -22,7 +22,7 @@ test = do
   sound <- getAudio text standardServer standardPort
   p@(_,_,s,_,_) <- prepareToPlay sound
   cmd <- return $  Sequence KPoseSequenceListOfPoses { animatedCharacter = "Amber"
-                                                     , startTime = 1000
+                                                     , startTime = 0
                                                      , fadeIn = Nothing
                                                      , fadeOut = Nothing
                                                      , timeWarp = Nothing
@@ -34,19 +34,38 @@ test = do
 
 stdKPoseSequence = KPoseSequenceListOfPoses "Amber" 0 Nothing Nothing Nothing []
 
-square = do
-  c <- return (TimeReset `mappend` cmd)
-  print c
-  standardAct c where
-  cmd = Sequence $ stdKPoseSequence{poses = ps}
---  ps = [rest]
+squareGesture = Sequence $ stdKPoseSequence{poses = ps} where
   ps = traceTrajectory LHand LArm points 4000
   points = [Vector3D (-0.5) (-0.2) (-0.5),
             Vector3D 0.5 (-0.2) (-0.5),
             Vector3D 0.5 (-0.2) 0.5,
             Vector3D (-0.5) (-0.2) 0.5]
+           
+-- | 'synchronize text animations delayAnimation delaySpeech' takes care of scheduling the animations and the sound playback, together with generating the lips animation. 'delayAnimation' and 'delaySpeech' are two extimates of the delay that needs to be applied to have a synchronized output
+synchronize :: String -- ^ the text to be spoken
+               -> [Command] -- ^ the animations that go together with the text
+               -> Integer -- ^ the delay to be applied to the animation
+               -> Integer -- ^ the delay to be applied to the speech
+               -> IO ()
+synchronize text animations delayAnimation delaySpeech = do
+  mapFile <- return "../data/phoneme-viseme.map" -- getDataFileName "phoneme-viseme.map"
+  m <- readFile mapFile
+  ps <- getPhonemesDurations text standardServer standardPort
+  sound <- getAudio text standardServer standardPort
+  p@(_,_,s,_,_) <- prepareToPlay sound
+  cmd <- return $  Sequence KPoseSequenceListOfPoses { animatedCharacter = "Amber"
+                                                     , startTime = 0
+                                                     , fadeIn = Nothing
+                                                     , fadeOut = Nothing
+                                                     , timeWarp = Nothing
+                                                     , poses = generateMorphKeysSequence ps (read m)}
+  executeInParallel [waitMillis delayAnimation >> 
+                     standardAct (TimeReset `mappend` cmd `mappend` (mconcat animations))
+                    , waitMillis delaySound >> play [s] >> waitForSource s >> cleanUp p]
+  
+  
 
 prerec :: [KPose]
 prerec = map (\(p,t) -> KPosePreRecorded (t*1000) (Just 1000) RHand p) $ zip (enumFromTo minBound maxBound) [1..]
 
-main = standardAct $ Sequence $ stdKPoseSequence{poses = prerec}
+main = standardAct $ TimeReset `mappend` (Sequence $ stdKPoseSequence{poses = prerec})
